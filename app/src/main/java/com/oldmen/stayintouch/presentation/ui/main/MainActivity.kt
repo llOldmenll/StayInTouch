5bebb41801ad917e2a8347b4ec0e6dc27fc92308
llOldmenll <shark7733@gmail.com>
2018-06-27 11:16:55 +0300

    - simple backdrop added

--- app/src/main/java/com/oldmen/stayintouch/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/oldmen/stayintouch/presentation/ui/main/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.oldmen.stayintouch.presentation.ui.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import com.arellomobile.mvp.MvpAppCompatActivity
 import com.oldmen.stayintouch.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MvpAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(app_bar)
+        app_bar.setNavigationOnClickListener(NavigationIconClickListener(
+                this,
+                articles_list,
+                AccelerateDecelerateInterpolator(),
+                getDrawable(R.drawable.drawer_open_icon), // Menu open icon
+                getDrawable(R.drawable.drawer_close_icon))) // Menu close icon
+
     }
 }
